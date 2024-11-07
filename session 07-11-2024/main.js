f144c0caa5b8934f6b0170451cdb4ad114c3ef05
salman-itShaala <salman.shaikh@itshaala.com>
2024-11-07 11:37:29 +0530

    added session from 7th

--- session 07-11-2024/main.js
+++ session 07-11-2024/main.js
@@ -30,3 +30,45 @@ let myIntro = "Im " + name + " and I live in " + city;
 let intro = `Im ${name} and I live in ${city}`;
 
 console.log(intro);
+
+// bigint
+
+let largerNum = 12n;
+
+console.log(typeof largerNum);
+
+let num3 = 9876543219876541n;
+
+let num4 = 9876543219876542n;
+
+console.log(num3 == num4);
+
+// 5 object
+
+let person = {
+  firstName: "salman",
+  lastName: "shaikh",
+  age: 45,
+};
+
+console.log(typeof person);
+
+// 6 undefined
+
+let num5; // undefined
+
+console.log(typeof num5); // undefined
+
+// 7. null
+
+let num6 = null;
+
+console.log(typeof num6); // object
+
+// 8. symbol
+
+let unique1 = Symbol("this is sybmol");
+
+let unique2 = Symbol("this is sybmol");
+
+console.log(typeof unique2); // symbol
