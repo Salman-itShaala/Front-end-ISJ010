1ae2a7877825e4761535f8625fd9101db3bfd40f
salman-itShaala <salman.shaikh@itshaala.com>
2024-11-07 11:03:09 +0530

    added session from 7th and 6th

---
+++ session 06-11-2024/script.js
@@ -0,0 +1,16 @@
+let isAbove18 = false;
+
+console.log(typeof isAbove18);
+
+// string using double inverted commas
+let name = "salman";
+
+// string using single inverted commas
+let city = "pune";
+
+// string using backticks
+let classes = `it shaala`;
+
+console.log(typeof name);
+console.log(typeof city);
+console.log(typeof classes);
---
+++ session 07-11-2024/main.js
@@ -0,0 +1,32 @@
+// data types in js
+
+// 8 data types in js
+
+// 1. number
+
+let num1 = 10;
+let num2 = 10.22;
+
+console.log(typeof num2);
+
+// 2. boolean
+
+// true,  false
+
+// 3. string
+
+// ""
+
+let name = "salman2";
+
+// ''
+
+let city = "pune";
+
+let myIntro = "Im " + name + " and I live in " + city;
+
+// `` -> backticks, template literal
+
+let intro = `Im ${name} and I live in ${city}`;
+
+console.log(intro);
